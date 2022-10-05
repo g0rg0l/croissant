6ebed511f25877ae721be7f75e28534fcdd6497f
g0rg0l <g0rgol@yandex.ru>
2022-10-06 00:15:11 +0300

    -

--- Entity/Player/Inventory/Inventory.cpp
+++ Entity/Player/Inventory/Inventory.cpp
@@ -1,6 +1,53 @@
 #include "Inventory.h"
 
-void Inventory::draw(sf::RenderTarget &target, sf::RenderStates states) const
-{
 
+
+Item *Inventory::takeItem(int index)
+{
+    Item* temp;
+
+    temp = inv[index];
+    inv[index] = nullptr;
+
+    return temp;
+}
+
+void Inventory::putItem(Item *item, int index) {inv[index] = item;}
+
+bool Inventory::correctSpecification(Item* item, int index)
+{
+    if (item->getSpecification() == "helmet")
+    {
+        return (index >= 4 && index <= 22) || index == 0;
+    }
+    if (item->getSpecification() == "chest plate")
+    {
+        return (index >= 4 && index <= 22) || index == 1;
+    }
+    if (item->getSpecification() == "leggings")
+    {
+        return (index >= 4 && index <= 22) || index == 2;
+    }
+    if (item->getSpecification() == "boots")
+    {
+        return (index >= 4 && index <= 22) || index == 3;
+    }
+
+    std::cout << "no such item specification as " << item->getSpecification() << std::endl;
+    return false;
+}
+
+bool Inventory::isIconEmpty(int index) {return inv[index] == nullptr;}
+
+int Inventory::findEmpty(Item *item)
+{
+    for (int i = 0; inv.size(); ++i)
+    {
+        if (!inv[i] && correctSpecification(item, i))
+        {
+            return i;
+        }
+    }
+
+    return -1;
 }
--- Entity/Player/Inventory/Inventory.h
+++ Entity/Player/Inventory/Inventory.h
@@ -6,27 +6,26 @@
 
 #include "../../../Items/Item.h"
 
-class Inventory : public sf::Drawable
+class Inventory
 {
-
 public:
     Inventory() = default;
 
 public:
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-
-public:
     std::unordered_map<int, Item*> inv = {
+            /* Экипировка */
             {0, new Armor("helmet", "basic helmet", 10, 100)},
             {1, new Armor("chest plate", "basic chest plate", 10, 100)},
             {2, new Armor("leggings", "basic leggings", 10, 100)},
             {3, new Armor("boots", "basic boots", 10, 100)},
 
+            /* Хот бар */
             {4, nullptr},
             {5, nullptr},
             {6, new Armor("boots", "basic boots", 10, 100)},
             {7, new Armor("helmet", "simple helmet", 15, 200)},
 
+            /* Ячейки */
             {8, nullptr},
             {9, nullptr},
             {10, nullptr},
@@ -45,62 +44,11 @@ public:
     };
 
 public:
-    Item* takeItem(int index)
-    {
-        Item* temp;
-
-        temp = inv[index];
-        inv[index] = nullptr;
-
-        return temp;
-    }
-
-    void putItem(Item* item, int index)
-    {
-        inv[index] = item;
-    }
-
-    bool correctSpecification(Item* item, int index)
-    {
-        if (item->getSpecification() == "helmet")
-        {
-            return (index >= 4 && index <= 22) || index == 0;
-        }
-        if (item->getSpecification() == "chest plate")
-        {
-            return (index >= 4 && index <= 22) || index == 1;
-        }
-        if (item->getSpecification() == "leggings")
-        {
-            return (index >= 4 && index <= 22) || index == 2;
-        }
-        if (item->getSpecification() == "boots")
-        {
-            return (index >= 4 && index <= 22) || index == 3;
-        }
-
-        std::cout << "no such item specification as " << item->getSpecification() << std::endl;
-        return false;
-    }
-
-    bool isIconEmpty(int index)
-    {
-        return inv[index] == nullptr;
-    }
-
-    int findEmpty(Item* item)
-    {
-        for (int i = 0; inv.size(); ++i)
-        {
-            if (!inv[i] && correctSpecification(item, i))
-            {
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
+    Item* takeItem(int index);
+    void putItem(Item* item, int index);
+    bool correctSpecification(Item* item, int index);
+    bool isIconEmpty(int index);
+    int findEmpty(Item* item);
 };
 
 
